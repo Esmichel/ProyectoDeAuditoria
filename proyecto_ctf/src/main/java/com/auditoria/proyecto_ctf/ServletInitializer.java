7e5319eb17366c36a7119b1c5eb9f7e2f6dfcefc
Davomeist <svequiza@gmail.com>
2024-11-26 18:26:19 +0100

    PrimerCommitDeLaWeb

---
+++ proyecto_ctf/src/main/java/com/auditoria/proyecto_ctf/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.auditoria.proyecto_ctf;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ProyectoCtfApplication.class);
+	}
+
+}
---
+++ proyecto_ctf/src/test/java/com/auditoria/proyecto_ctf/ProyectoCtfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.auditoria.proyecto_ctf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoCtfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
