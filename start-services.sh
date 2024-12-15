71bbd3b187bf548b912a4cdeaca9bca670324427
Ander <ander.alonsom@gmail.com>
2024-12-15 11:12:25 +0100

    Si

---
+++ start-services.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Start PostgreSQL
+service postgresql start
+
+until pg_isready -h localhost -p 5432 -U postgres; do
+  echo "Esperando a PostgreSQL..."
+  sleep 2
+done
+
+# Start Apache (optional)
+service apache2 start
+
+# Start FTP service
+service vsftpd start
+
+# Run the Spring Boot application
+java -jar /app/app.jar
